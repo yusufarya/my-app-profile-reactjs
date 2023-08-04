2668258a1e8ee8192bc20f20ef6c24cb323632c0
yusufarya <aryaherby29nov2k@gmail.com>
2023-08-04 22:52:24 +0700

    fixed navbar responsive 1

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,4 +1,4 @@
-import logoHeader from './../assets/img/header/robot1.jpg' 
+import logoHeader from './../assets/img/header/robot1.jpg'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles 
 import { Fragment, useEffect, useState } from 'react';
@@ -23,9 +23,9 @@ var sectionStyle = {
 
 function Home() {
 
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
 
-    useEffect(() => {  
+    useEffect(() => {
         return () => {
             setLoading(false)
         };
@@ -33,39 +33,43 @@ function Home() {
 
     return (
         <>
-        {loading &&
-            <ContentLoader 
-                speed={2}
-                width='100%'
-                height={260}
-                viewBox="0 0 780 400"
-                backgroundColor="#f3f3f3"
-                foregroundColor="#ecebeb">
+            {loading &&
+                <ContentLoader
+                    speed={2}
+                    width='100%'
+                    height={260}
+                    viewBox="0 0 780 400"
+                    backgroundColor="#f3f3f3"
+                    foregroundColor="#ecebeb">
 
-                <rect x="88" y="128" rx="3" ry="3" width="75%" height="56" /> 
-                <rect x="0" y="218" rx="3" ry="3" width="100%" height="10" /> 
-                <rect x="0" y="238" rx="3" ry="3" width="100%" height="10" />
+                    <rect x="88" y="128" rx="3" ry="3" width="75%" height="56" />
+                    <rect x="0" y="218" rx="3" ry="3" width="100%" height="10" />
+                    <rect x="0" y="238" rx="3" ry="3" width="100%" height="10" />
 
-            </ContentLoader>
-        }
-        {!loading &&
-            <Fragment>
-                <div className="justify-center mx-3">
-                    {/* <img src={logoHeader} alt="logo_header" className='flex bg-cover bg-fixed bg-center' /> */}
-                    <div style={sectionStyle}>
-                        <div className='p-5 grid justify-items-center opacity-100 brightness-100 '>
-                            <br />
-                            <h1 className='font-bold text-black mt-5 sticky top-600'> 
-                                Hello! I'am Yusuf.   
-                            </h1>
-                            <p className='text-justify text-black text-semibold text-xl'> Web developer who is passionate about building user-centric solutions.</p>
-                            <p className='flex justify-start'> See More ...</p>
+                </ContentLoader>
+            }
+            {!loading &&
+                <Fragment>
+                    <div className="justify-center mx-3"
+                        data-aos="fade-up"
+                        data-aos-offset="200"
+                        data-aos-delay="50"
+                        data-aos-duration="700"
+                        data-aos-easing="ease-in-out">
+                        <div style={sectionStyle}>
+                            <div className='p-5 grid justify-items-center opacity-100 brightness-100 '>
+                                <br />
+                                <h1 className='font-bold text-black mt-5 sticky top-600'>
+                                    Hello! I'am Yusuf.
+                                </h1>
+                                <p className='text-justify text-black text-semibold text-xl'> Web developer who is passionate about building user-centric solutions.</p>
+                                <p className='flex justify-start'> See More ...</p>
+                            </div>
                         </div>
+                        <br />
                     </div>
-                    <br />
-                </div>
-            </Fragment>
-        }
+                </Fragment>
+            }
 
         </>
     );
