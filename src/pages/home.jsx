566b8e37f34a2b071a1d0c469f95a17076b41ced
yusufarya <aryaherby29nov2k@gmail.com>
2024-09-12 17:22:16 +0700

    update url project medicare

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -100,7 +100,7 @@ function Home() {
               <h4 className="font-bold mt-3">Medicare</h4>
               <p>Aplikasi berbasis web untuk manajemen klinik</p>
               <a href="https://my.medicare.29digitec.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">See more...</a><br />
-              <a href="http://http://medicare.web.id/admin" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Visit project...</a>
+              <a href="http://medicare.web.id/admin" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Visit project...</a>
             </div>
             <div id="item-project" className="p-4 bg-gray-100 rounded-lg shadow-md">
               <img src={studio} alt="29 Studio project" className="w-full h-auto mb-2 rounded-md" />
